8d68870ec32b1720e48a84e4765640238b20a932
jojo5564 <s9811313@gmail.com>
2022-12-09 11:39:27 +0800

    v0.3

--- xlsb_to_xlsx/Form1.Designer.cs
+++ xlsb_to_xlsx/Form1.Designer.cs
@@ -31,13 +31,16 @@ namespace xlsb_to_xlsx
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(427, 12);
+            this.button1.Location = new System.Drawing.Point(430, 38);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,21 +50,52 @@ namespace xlsb_to_xlsx
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 46);
+            this.listBox1.Location = new System.Drawing.Point(12, 66);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(497, 160);
             this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(316, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output Location : ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(430, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 219);
+            this.ClientSize = new System.Drawing.Size(521, 236);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "xlsb to xlsx";
+            this.Text = "xlsb to xlsx v0.3";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +103,9 @@ namespace xlsb_to_xlsx
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- xlsb_to_xlsx/Form1.cs
+++ xlsb_to_xlsx/Form1.cs
@@ -16,69 +16,125 @@ namespace xlsb_to_xlsx
 {
     public partial class Form1 : Form
     {
-        private string Path = "";
+        private string OutputPath = "";
+        private readonly IniManager iniManager = new IniManager(Application.StartupPath + "\\Config.ini");
 
         public Form1()
         {
             InitializeComponent();
+
+            textBox1.Text = iniManager.ReadIniFile("Location", "Output", " ");
+            OutputPath = iniManager.ReadIniFile("Location", "Output", " ");
         }
         
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
             listBox1.Items.Clear();
+            if (OutputPath == "")
+            {
+                MessageBox.Show("Error : Output location is null");
+                button1.Enabled = true;
+                button2.Enabled = true;
+                return;
+            }
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Multiselect = true;
             dialog.Title = "Please select .xlsb files";
             dialog.Filter = "*.xlsb|*.xlsb";
-            string[] files;
-
-
             if (dialog.ShowDialog() == DialogResult.OK)
             {
-                FolderBrowserDialog savedialog = new FolderBrowserDialog();
-                savedialog.ShowDialog();
-                Path = savedialog.SelectedPath;
-
-                if(Path == "")
-                {
-                    this.Enabled = true;
-                    return;
-                }
-
-                files = dialog.FileNames;
-                listBox1.Items.Add("Total : " + files.Length + " files");
-                foreach (var file in files)
-                {
-                    listBox1.Items.Add("Converting : " + file.Split('\\').Last());
-                    try
-                    {
-                        Thread t1 = new Thread(new ParameterizedThreadStart(Convert));
-                        t1.Start(file);
-                        while (t1.ThreadState == ThreadState.Running)
-                        {
-                            this.Update();
-                            Thread.Sleep(1);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        listBox1.Items.Add("Failed");
-                    }
-                }
-                listBox1.Items.Add("succeed");
+                Thread t1 = new Thread(new ParameterizedThreadStart(Convert));
+                t1.Start(dialog.FileNames);
             }
-            this.Enabled = true;
-
         }
 
-        private void Convert(object objs)
+        private void button2_Click(object sender, EventArgs e)
         {
-            string file = (String)objs;
-            var workbook = new Workbook(file);
-            string newFileName = file.Split(new[] { ".xlsb" }, StringSplitOptions.None)[0] + ".xlsx";
-            workbook.Save(Path + '\\' + newFileName.Split('\\').Last());
+            var tmp = GetSavePath();
+            if (tmp != "")
+            {
+                OutputPath = tmp;
+                textBox1.Text = tmp;
+                iniManager.WriteIniFile("Location", "Output", tmp);
+            }
         }
+
+        private string GetSavePath()
+        {
+            FolderBrowserDialog savedialog = new FolderBrowserDialog();
+            savedialog.ShowDialog();
+            return savedialog.SelectedPath;
+        }
+
+        private void Convert(object obj)
+        {
+            string[] files = (string[])obj;
+            ListBoxControl(listBox1, "Add", "Total : " + files.Length + " files");
+            for (int i = 0; i < files.Length; i++)
+            {
+                ListBoxControl(listBox1, "Add", String.Format("Converting {0}/{1} : {2} ...", i + 1, files.Length, files[i].Split('\\').Last()));
+                
+                try
+                {
+                    XlsbToXlsx(files[i]);
+                }
+                catch (Exception ex)
+                {
+                    ListBoxControl(listBox1, "Add", String.Format("fail {0}/{1} : {2} ...", i + 1, files.Length, files[i].Split('\\').Last()));
+                }
+            }
+            ListBoxControl(listBox1, "Add", "succeed");
+            ButtonControl(button1, true);
+            ButtonControl(button2, true);
+        }
+
+        private void XlsbToXlsx(object obj)
+        {
+            string file = (String)obj;
+            var workbook = new Workbook(file);
+            string newFileName = (file.Split(new[] { ".xlsb" }, StringSplitOptions.None)[0] + ".xlsx").Split('\\').Last();
+            workbook.Save(OutputPath + '\\' + newFileName);
+        }
+
+        #region delegate
+        private delegate void ListBoxControlCallback(ListBox listBox, string command, string str);
+        public delegate void ButtonControlCallback(Button button, bool enable);
+
+        public void ListBoxControl(ListBox listBox, string command, string str)
+        {
+            if (listBox.InvokeRequired)
+            {
+                ListBoxControlCallback d = new ListBoxControlCallback(ListBoxControl);
+                this.Invoke(d, new object[] { listBox, command, str });
+            }
+            else
+            {
+                if (command == "Add")
+                {
+                    listBox.Items.Add(str);
+                    listBox.TopIndex = listBox.Items.Count - 1;
+                }
+                else if (command == "Clear")
+                {
+                    listBox.Items.Clear();
+                }
+            }
+        }
+        public void ButtonControl(Button button, bool enable)
+        {
+            if (button.InvokeRequired)
+            {
+                ButtonControlCallback d = new ButtonControlCallback(ButtonControl);
+                this.Invoke(d, new object[] { button, enable });
+            }
+            else
+            {
+                button.Enabled = enable;
+            }
+        }
+        #endregion
 
     }
 }
